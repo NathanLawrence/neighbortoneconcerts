b435df5ac50d84a2067e9e8731d845d9d605b54b
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-02-13 15:57:48 -0600

    Fixing all my mistakes. Adding top nav test.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var gulp = require('gulp'),
 	less = require('gulp-less');
 
 gulp.task('less', function(){
-	return gulp.src('components/less/styles.less')
+	return gulp.src('less/styles.less')
     .pipe(less({}))
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch',function(){
