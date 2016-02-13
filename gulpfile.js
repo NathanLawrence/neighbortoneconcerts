546185ea588f1bf1ee2226a4e0d1394f4a75e0f8
Nathan Lawrence <nathan.Lawrence.barrick@gmail.com>
2016-02-13 15:32:21 -0600

    Starting up some awesome LESS!

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	less = require('gulp-less');
+
+gulp.task('less', ['fonts'], function(){
+	return gulp.src('/less/styles.less')
+    .pipe(less({}))
+    .pipe(gulp.dest('/css'));
+});
+
+gulp.task('watch',function(){
+	gutil.log('Gulp will say that this task has finished, but don\'t believe its dirty lies.');
+	gutil.log('Hit \^c to actually exit watch mode.');
+});
